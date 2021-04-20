43bf709c772cc37662e9962ed5c26f8ca6759363
David <dtoepfer@csustan.edu>
2021-04-20 14:25:21 -0700

    Added variables

---
+++ MealControllerClass.py
@@ -0,0 +1,14 @@
+# Meal Controller Class
+
+MealTempDate = ""       # Date of meal to be put into file
+MealTempTime = ""       # Time of meal //
+MealTempName = ""       # What the user ate to be put into file
+MealTempCal = ""        # Calories of meal to be put into file
+MealTempFat = ""        # Fat of meal //
+MealTempSug = ""        # Sugars //
+MealTempChol = ""       # Cholesterol //
+MealTempSod = ""        # Sodium //
+MealTempProt = ""       # Protein //
+MealNotes = ""          # Any notes that the user would like to make about the  						     meal
+CalConsumedCount = ""   # The total sum of the days calories, resets at midnight
+CalLeft = ""            # Calories Left for the day
