de09ac96aee58c0dfd109e3a5881f48a45a27843
David <dtoepfer@csustan.edu>
2021-04-20 18:51:21 -0700

    started structure

--- MealControllerClass.py
+++ MealControllerClass.py
@@ -1,4 +1,6 @@
 # Meal Controller Class
+from PyQt5.QtWidgets import *
+import sys
 
 MealTempDate = ""       # Date of meal to be put into file
 MealTempTime = ""       # Time of meal //
@@ -12,3 +14,31 @@ MealTempProt = ""       # Protein //
 MealNotes = ""          # Any notes that the user would like to make about the  						     meal
 CalConsumedCount = ""   # The total sum of the days calories, resets at midnight
 CalLeft = ""            # Calories Left for the day
+
+
+@event(AddAMealBtn, Pressed)
+def enter_a_meal(MealTempDate, MealTempTime, MealTempName, MealTempCal, MealTempFat,
+    MealTempSug, MealTempChol, MealTempSod, MealTempProt, MealNotes):
+    screen = MealInfo()
+    screen.show()
+
+    MealTempDate
+    MealTempTime
+    MealTempName
+    MealTempCal
+    MealTempFat
+    MealTempSug
+    MealTempChol
+    MealTempSod
+    MealTempProt
+    MealNotes
+    MealTempDate
+    MealTempTime
+    MealTempName
+    MealTempCal
+    MealTempFat
+    MealTempSug
+    MealTempChol
+    MealTempSod
+    MealTempProt
+    MealNotes
