338fcabaf6a6402c66763a4e40b0f20e50712dba
David <dtoepfer@csustan.edu>
2021-04-22 14:19:06 -0700

    added eq to calculate calories

--- MealControllerClass.py
+++ MealControllerClass.py
@@ -1,43 +1,26 @@
 # Meal Controller Class
-class MealControllerClass:
-    from PyQt5.QtWidgets import *
-    import sys
-    from bs4 import BeautifulSoup
+# David Toepfer
 
-    # Temporary variables to hold users input between UI and database.
-    MealTempDate = ""  # Date of meal to be put into file
-    MealTempTime = ""  # Time of meal //
-    MealTempName = ""  # What the user ate to be put into file
-    MealTempCal = ""  # Calories of meal to be put into file
-    MealTempFat = ""  # Fat of meal //
-    MealTempSug = ""  # Sugars //
-    MealTempChol = ""  # Cholesterol //
-    MealTempSod = ""  # Sodium //
-    MealTempProt = ""  # Protein //
-    MealNotes = ""  # Any notes that the user would like to make about the  						     meal
+from classes import *
+
+
+class MealControllerClass:
+    # Temporary variables to hold changing variables
     CalConsumedCount = ""  # The total sum of the days calories, resets at midnight
     CalLeft = ""  # Calories Left for the day
 
     # When Add a meal is pressed
 
-    def add_the_meal(MealTempDate, MealTempTime, MealTempName, MealTempCal, MealTempFat,
-                     MealTempSug, MealTempChol, MealTempSod, MealTempProt, MealNotes):
+    def add_the_meal(self, CalConsumedCount, CalLeft):
+        new_meal = Meal(MealNameLbl.text(), MealDateLbl.text(), MealCalLbl.text(), MealFatLbl.text(),
+                       MealSugLbl.text(), MealCholLbl.text(), MealSodLbl.text(), MealProtLbl.text(), MealTimeLbl.text(),
+                       MealNotesLbl.text())
 
+        new_meal.AddMealDatabase()  # temps to file.
 
-        MealTempDate = MealDateLbl.text()   # These lines take the users input into the variable
-        MealTempTime = MealTimeLbl.text()
-        MealTempName = MealNameLbl.text()
-        MealTempCal = MealCalLbl.text()
-        MealTempFat = MealFatLbl.text()
-        MealTempSug = MealSugLbl.text()
-        MealTempChol = MealCholLbl.text()
-        MealTempSod = MealSodLbl.text()
-        MealTempProt = MealProtLbl.text()
-        MealNotes = MealNotesLbl.text()
+        CalConsumedCount = CalConsumedCount + MealCalLbl.text    # Calculates the total calories consumed for the day
+        CalLeft = TargetCal - CalConsumedCount              # Calculates the amount of calories left for the day
 
-        # Need to add commands to add temps to file.
 
         screen = MealWindow()  # returns to meals page
         screen.show()
-
-        
