691559e997d910e270a4af982fd4687655a0eb4c
W1serV1ser <ben@witort.com>
2021-04-22 16:27:20 -0700

    Added comments and LoadMeals method

--- classes.py
+++ classes.py
@@ -1,12 +1,26 @@
+# Ben Witort
+
 import xml.etree.ElementTree as Xml
 
+# Used to hold all meals in database while user is in Meal window
 MealList = []
 
+# Loads in all of the meals from the meals database into MealList
+def LoadMeals():
+    file = Xml.parse("databases/meals.xml")
+    meals = file.getroot()
+    for meal in meals:
+        MealList.append(Meal(meal.find("Name").text, meal.find("Date").text, meal.find("Calories").text,
+                             meal.find("Fat").text, meal.find("Sugar").text, meal.find("Cholesterol").text,
+                             meal.find("Sodium").text, meal.find("Protein").text, meal.find("Time").text,
+                             meal.find("Notes").text))
+
+# Basic class for all entries (not necessary?)
 class Entry:
     Name = ""
     Date = ""
 
-
+# Class for meal entries
 class Meal(Entry):
     Calories = ""
     Fat = ""
@@ -29,7 +43,7 @@ class Meal(Entry):
         self.Time = MealTime
         self.Notes = MealNotes
 
-    @staticmethod
+    # Puts the meal into the meal database
     def AddMealDatabase(self):
         file = Xml.parse("databases/meals.xml")
         meals = file.getroot()
@@ -57,7 +71,6 @@ class Meal(Entry):
         mealNotes.text = self.Notes
         file.write("databases/meals.xml")
 
-    @staticmethod
+    # Puts the meal into MealList
     def AddMealList(self):
         MealList.append(self)
-
