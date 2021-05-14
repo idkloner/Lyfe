cf6d6c99c5489ddc81680bccf5de4b1fea02f94f
agthomas95 <80067325+agthomas95@users.noreply.github.com>
2021-05-13 23:39:50 -0700

    Added and moved a function

--- MealClass.py
+++ MealClass.py
@@ -1,9 +1,17 @@
-# Ben Witort
+# Ben Witort, Anthony Thomas
 
 # Library that handles creating and interacting with XML files
 import xml.etree.ElementTree as Xml
 # Library needed for creating database directory
 import os
+
+# 5/13/21:      Moved CheckForDatabase() outside of Meal class so that MainMealWindow can call it without having to put
+#               in meal values not yet initialized and check that the database is created.
+
+#               Added ChangeCalorieGoal() that will take the new calorie value from the user input in MainMealWindow
+#               and change the value stored in the database. If element hasn't been created yet within the database, it
+#               will create one instead.
+#               -agthomas95
 
 # Used to hold all meals in database while user is in Meal window
 MealList = []
@@ -18,10 +26,13 @@ def LoadMeals():
         MealList.clear()
 
     for meal in meals:
-        MealList.append(Meal(meal.find("Name").text, meal.find("Date").text, meal.find("Calories").text,
-                             meal.find("Fat").text, meal.find("Sugar").text, meal.find("Cholesterol").text,
-                             meal.find("Sodium").text, meal.find("Protein").text, meal.find("Time").text,
-                             meal.find("Notes").text))
+        try:
+            MealList.append(Meal(meal.find("Name").text, meal.find("Date").text, meal.find("Calories").text,
+                                 meal.find("Fat").text, meal.find("Sugar").text, meal.find("Cholesterol").text,
+                                 meal.find("Sodium").text, meal.find("Protein").text, meal.find("Time").text,
+                                 meal.find("Notes").text))
+        except:
+            continue
 
 # Finds the proper index for the new meal to be added to database based on Date and Time
 def FindInsertIndex(Date, Time, DatabaseRoot):  #changed values to use split to get the numbers between each symbol.
@@ -36,34 +47,62 @@ def FindInsertIndex(Date, Time, DatabaseRoot):  #changed values to use split to 
 
     counter = 0
     for meal in DatabaseRoot:
-        CurrentMealDate = meal[0].text
-        CurrentMealTime = meal[1].text
-        CurrentMealMonth = CurrentMealDate.split("/")[0]
-        CurrentMealDay = CurrentMealDate.split("/")[1]
-        CurrentMealYear = CurrentMealDate.split("/")[2]
-        CurrentMealHour = CurrentMealTime.split(":")[0]
-        CurrentMealMinute = CurrentMealTime[CurrentMealTime.find(":")+1 : CurrentMealTime.find(" ")]
-        CurrentMealTimeFrame = CurrentMealTime.split(" ")[1]
-        if CurrentMealTimeFrame == "PM": CurrentMealHour = str(int(MealHour) + 12)  #Used for comparing time by converting to 24-hour time
+        try:
+            CurrentMealDate = meal[0].text
+            CurrentMealTime = meal[1].text
+            CurrentMealMonth = CurrentMealDate.split("/")[0]
+            CurrentMealDay = CurrentMealDate.split("/")[1]
+            CurrentMealYear = CurrentMealDate.split("/")[2]
+            CurrentMealHour = CurrentMealTime.split(":")[0]
+            CurrentMealMinute = CurrentMealTime[CurrentMealTime.find(":")+1 : CurrentMealTime.find(" ")]
+            CurrentMealTimeFrame = CurrentMealTime.split(" ")[1]
+            if CurrentMealTimeFrame == "PM": CurrentMealHour = str(int(MealHour) + 12)  # Used for comparing time by converting to 24-hour time
 
-        if(int(CurrentMealYear) > int(DateInfo["Year"])):
-            counter += 1
-            continue
-        if(int(CurrentMealMonth) > int(DateInfo["Month"])):
-            counter += 1
-            continue
-        if(int(CurrentMealDay) > int(DateInfo["Day"])):
-            counter += 1
-            continue
-        if(int(CurrentMealDay) == int(DateInfo["Day"])):
-            if(int(CurrentMealHour) > int(DateInfo["Hour"])):
+            if(int(CurrentMealYear) > int(DateInfo["Year"])):
                 counter += 1
                 continue
-            if(int(CurrentMealMinute) > int(DateInfo["Minute"])):
+            if(int(CurrentMealMonth) > int(DateInfo["Month"])):
                 counter += 1
                 continue
+            if(int(CurrentMealDay) > int(DateInfo["Day"])):
+                counter += 1
+                continue
+            if(int(CurrentMealDay) == int(DateInfo["Day"])):
+                if(int(CurrentMealHour) > int(DateInfo["Hour"])):
+                    counter += 1
+                    continue
+                if(int(CurrentMealMinute) > int(DateInfo["Minute"])):
+                    counter += 1
+                    continue
+        except:
+            continue
         break
     return counter
+
+def CheckForDatabase():
+    # If the meal database doesn't exist, create it
+    try:
+        file = Xml.parse("databases/meals.xml")
+    except:
+        database = Xml.ElementTree(Xml.Element("Meals"))
+        if (not os.path.isdir("databases")):
+            os.makedirs("databases")
+        with open("databases/meals.xml", "wb") as NewDatabase:
+            database.write(NewDatabase)
+        ChangeCalorieGoal(0)
+
+def ChangeCalorieGoal(calories):
+    try:
+        file = Xml.parse("databases/meals.xml")
+        file.getroot().find("CalorieGoal/Goal").text = str(calories)
+        file.write("databases/meals.xml")
+    except:
+        root = file.getroot()
+        calorieElement = Xml.Element("CalorieGoal")
+        calorieSubElement = Xml.SubElement(calorieElement, "Goal")
+        calorieSubElement.text = str(calories)
+        root.insert(0, calorieElement)
+        file.write("databases/meals.xml")
 
 
 # Class for meal entries
@@ -84,16 +123,8 @@ class Meal:
 
     # Puts the meal into the meal database
     def AddMealDatabase(self):
-        # If the meal database doesn't exist, create it
-        try:
-            file = Xml.parse("databases/meals.xml")
-        except:
-            database = Xml.ElementTree(Xml.Element("Meals"))
-            if(not os.path.isdir("databases")):
-                os.makedirs("databases")
-            with open("databases/meals.xml", "wb") as NewDatabase:
-                database.write(NewDatabase)
-            file = Xml.parse("databases/meals.xml")
+        CheckForDatabase()
+        file = Xml.parse("databases/meals.xml")
         meals = file.getroot()
         newMeal = Xml.Element("Meal")
         index = FindInsertIndex(self.Date, self.Time, meals)
