82e50798d8f4972ddd1db075136358a7e27e84ac
agthomas95 <80067325+agthomas95@users.noreply.github.com>
2021-05-13 23:34:46 -0700

    Implemented target calorie change

--- MainMealWindow.py
+++ MainMealWindow.py
@@ -3,17 +3,28 @@
 # Generates the UI that shows the list of meals added, a pie graph to show calories consumed for the day and remaining
 # calories before exceeding the user's target calories, and a button to add a new meal.
 
-# 5/2/21:   Added function for "add a meal" button to pop up a new window
-#           -agthomas95
+# All changes made by agthomas95 (Anthony Thomas) unless specified:
+# 5/2/21:       Added function for "add a meal" button to pop up a new window
 
-# 5/5/21:   Replaced the placeholder list text with function addMealsToList() that will take all meal information from
-#           the database and show the meal's name, calories, and time under its corresponding date. Function will also
-#           add the calories of each meal from today's date to be displayed in the pie chart.
-#           Updated the pie chart to show the updated calories consumed and how much left before exceeding the user's
-#           daily target calories, followed by the previous information.
-#           Back button now switches to the Lyfe main menu UI.
-#           Moved the Lyfe main window import to the function that'll switch to the Lyfe main menu UI to avoid an ImportError.
-#           -agthomas95
+
+# 5/5/21:       Replaced the placeholder list text with function addMealsToList() that will take all meal information from
+#               the database and show the meal's name, calories, and time under its corresponding date. Function will also
+#               add the calories of each meal from today's date to be displayed in the pie chart.
+
+#               Updated the pie chart to show the updated calories consumed and how much left before exceeding the user's
+#               daily target calories, followed by the previous information.
+
+#               Back button now switches to the Lyfe main menu UI.
+
+#               Moved the Lyfe main window import to the function that'll switch to the Lyfe main menu UI to avoid an ImportError.
+
+
+# 5/13/21:      Added a button to change the user's target calories and a text box to enter it. Entering a non-integer
+#               will prompt an error until a valid integer is entered.
+
+#               Added functions to check that the text box and error is not shown until prompted, and to change the
+#               calorie goal stored in the database that'll be called in this class to change the calorie text shown
+#               around the pie graph.
 
 from datetime import datetime
 
@@ -50,6 +61,20 @@ class Ui_MainMealWindow(object):
         self.MainMenuButton.setGeometry(QtCore.QRect(90, 510, 131, 51))
         self.MainMenuButton.setObjectName("MainMenuButton")
         self.MainMenuButton.clicked.connect(lambda: self.toLyfeMainWindow(MainMealWindow))
+        self.ChangeCalTargetButton = QtWidgets.QPushButton(self.centralwidget)
+        self.ChangeCalTargetButton.setGeometry(QtCore.QRect(400, 10, 180, 31))
+        self.ChangeCalTargetButton.setObjectName("ChangeTargetCalButton")
+        self.CalTargetTextBox = QtWidgets.QLineEdit(self.centralwidget)
+        self.CalTargetTextBox.setGeometry(QtCore.QRect(440, 50, 100, 21))
+        self.CalTargetTextBox.setObjectName("CalTargetTextBox")
+        self.CalTargetTextBox.setPlaceholderText("Daily calories")
+        self.CalTargetTextBox.setVisible(False)
+        self.CalTargetTextBox.returnPressed.connect(lambda: self.checkForCalTargetInt(self.CalTargetTextBox.text(), MainMealWindow))
+        self.ChangeCalTargetButton.clicked.connect(lambda: self.calTargetVisible())
+        self.CalTargetError = QtWidgets.QLabel(self.centralwidget)
+        self.CalTargetError.setGeometry(QtCore.QRect(435, 75, 140, 21))
+        self.CalTargetError.setObjectName("CalTargetError")
+        self.CalTargetError.setVisible(False)
         self.CurrentDateDisplay = QtWidgets.QLabel(self.centralwidget)
         self.CurrentDateDisplay.setGeometry(QtCore.QRect(20, 10, 271, 61))
         font = QtGui.QFont()
@@ -69,7 +94,7 @@ class Ui_MainMealWindow(object):
         try:  # skips adding meals to a list if database doesn't exist yet -agthomas95
             self.addMealsToList()
         except:
-            pass
+            CheckForDatabase()
 
         # pie chart creation
         self.pieChartWidget = QtWidgets.QWidget(self.centralwidget)
@@ -77,7 +102,7 @@ class Ui_MainMealWindow(object):
 
         self.calorieSeries = QPieSeries()   # class for creating pie chart
         self.calorieSeries.append("Calories consumed", MealControllerClass.CalConsumedCount)
-        self.calorieSeries.append("Calories left", MealControllerClass.TargetCal - MealControllerClass.CalConsumedCount)
+        self.calorieSeries.append("Calories left", int() - MealControllerClass.CalConsumedCount)
 
         self.calorieSlice = self.calorieSeries.slices()[0]  # slice for calories consumed
         self.calorieSlice.setPen(QPen(Qt.darkGray, 1))
@@ -144,11 +169,24 @@ class Ui_MainMealWindow(object):
                 if meal.Date == datetime.today().strftime("%m/%d/%Y"):
                     MealControllerClass.CalConsumedCount += int(meal.Calories)
 
-        # updates daily calories consumed and remaining calories with target calories around pie chart
-        self.caloriesText.setText(_translate("MainMealWindow", "Calories consumed: " +str(MealControllerClass.CalConsumedCount) +
-                                             " calories\nCalories left: " + str(MealControllerClass.TargetCal - MealControllerClass.CalConsumedCount) +
-                                             " calories"))
-        self.dailyGoalText.setText(_translate("MainMealWindow", "Daily goal:\n" + str(MealControllerClass.TargetCal) + " calories"))
+        self.retranslateUi(MainMealWindow)
+
+    def calTargetVisible(self):
+        if self.CalTargetTextBox.isVisible():
+            self.CalTargetTextBox.setVisible(False)
+            self.CalTargetError.setVisible(False)
+        else:
+            self.CalTargetTextBox.setVisible(True)
+
+    def checkForCalTargetInt(self, value, MainMealWindow):
+        try:
+            intTest = int(value)
+            ChangeCalorieGoal(value)
+            self.CalTargetTextBox.setVisible(False)
+            self.CalTargetError.setVisible(False)
+            self.retranslateUi(MainMealWindow)
+        except:
+            self.CalTargetError.setVisible(True)
 
     # goes to the UI for adding a meal
     def toAddMeal(self, MainMealWindow):
@@ -176,11 +214,18 @@ class Ui_MainMealWindow(object):
         self.listWidget.setSortingEnabled(__sortingEnabled)
         self.addMealButton.setText(_translate("MainMealWindow", "Add a meal"))
         self.MainMenuButton.setText(_translate("MainMealWindow", "Back"))
+        self.ChangeCalTargetButton.setText(_translate("MainMealWindow", "Change daily calories"))
+        self.CalTargetError.setText(_translate("MainMealWindow", "<html><head/><body><p style=\"color:red\"><span style=\" font-size:10pt;\">Enter a valid integer</span></p></body></html>"))
         currentDate = date.today()
         self.CurrentDateDisplay.setText(_translate("MainMealWindow", currentDate.strftime("%A, %B %d")))
-        self.caloriesText.setText(_translate("MainMealWindow", "Calories consumed: " + str(MealControllerClass.CalConsumedCount) +
-                                             " calories\nCalories left: " + str(MealControllerClass.TargetCal - MealControllerClass.CalConsumedCount) + " calories"))
-        self.dailyGoalText.setText(_translate("MainMealWindow", "Daily goal:\n" + str(MealControllerClass.TargetCal) + " calories"))
+        self.caloriesText.setText(_translate("MainMealWindow", "Calories consumed: " +
+                                             str(MealControllerClass.CalConsumedCount) +
+                                             " calories\nCalories left: " +
+                                             str(int(Xml.parse("databases/meals.xml").getroot().find("CalorieGoal/Goal").text) -
+                                                 MealControllerClass.CalConsumedCount) + " calories"))
+        self.dailyGoalText.setText(_translate("MainMealWindow", "Daily goal:\n" +
+                                              Xml.parse("databases/meals.xml").getroot().find("CalorieGoal/Goal").text +
+                                              " calories"))
 
 
 if __name__ == "__main__":
